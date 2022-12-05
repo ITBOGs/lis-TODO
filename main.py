7f163dd5981348fe5c878c500a0ef2df3875c131
Vladimir Sheenko <vovan072005@gmail.com>
2022-12-05 18:05:03 +0300

    Сделал подключение (отключение) к БД, но в случае неудачи он просто смирится, как-то надо сделать проверочку с повторным подключением и выучить английский:) Но я пока для себя все это.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
+
 from design_MainWindow import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui
+
+from sqlquery import SqlQuery
 
 
 class lis_todo(QMainWindow):
@@ -9,10 +13,22 @@ class lis_todo(QMainWindow):
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
 
+	def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+		sql_query.db_disconnect()
+
+	def showEvent(self, a0: QtGui.QShowEvent) -> None:
+		sql_query.db_connect()
+
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	window = lis_todo()
-	window.show()
 
+	sql_query = SqlQuery()
+
+
+	window.show()
 	sys.exit(app.exec())
+
+
+
---
+++ sqlquery.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+
+class SqlQuery:
+	def __init__(self):
+		super(SqlQuery, self).__init__()
+
+		self.connection = None
+		self.host = '127.0.0.1'
+		self.user = 'postgres'
+		self.password = '2210'
+		self.db_name = 'lis_todo'
+
+	def db_connect(self):
+		try:
+			self.connection = psycopg2.connect(
+				host=self.host,
+				user=self.user,
+				password=self.password,
+				database=self.db_name
+			)
+			print('[INFO] Successful database connection')
+
+		except Exception as _ex:
+			print('[INFO] Error while working with postgres (connection)', _ex)
+
+	def db_disconnect(self):
+		try:
+			self.connection.close()
+			print('[INFO] Database disconnected')
+
+		except Exception as _ex:
+			print('[INFO] Error while working with postgres (disconnection)', _ex)
